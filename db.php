8daac4029871771ccf2c8560e0466a983547dc3a
adoslic <adoslic@etfos.hr>
2018-05-21 18:15:15 +0200

    bnagjakh

---
+++ db.php
@@ -0,0 +1,31 @@
+<?php
+$host = "lv3ruapp-mysqldbserver.mysql.database.azure.com";
+$user = "ruapUser@lv3ruapp-mysqldbserver";
+$pwd = "plemena1.";
+$db = "mysqldatabase39229";
+ $conn = mysqli_connect($host,$user,$pwd);
+ // Check connection
+ if (mysqli_connect_errno())
+ {
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }
+ else {
+ echo 'Connected successfully';
+ }
+ $sql = 'CREATE Database ruapdb';
+ if ($conn->query($sql) === TRUE) {
+ echo "Database created successfully";
+ } else {
+ echo "Error creating database: " . $conn->error;
+ }
+ mysqli_select_db($conn,$db);
+ $sql_create = "CREATE TABLE registration_tbl(id INT NOT NULL
+ AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(30), email
+ VARCHAR(30), date DATE);";
+ if ($conn->query($sql_create) === TRUE) {
+ echo "Table registration_tbl created successfully";
+ } else {
+ echo "Error creating table: " . $conn->error;
+ }
+ $conn->close();
+?>
